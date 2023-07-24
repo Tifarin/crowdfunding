ba2c408a551eb6fcc8eec5e961f541c5a15fe92e
tifarin <latifahlatifah443@gmail.com>
2023-07-24 22:11:41 +0700

    cms : create user

--- api/user/input.go
+++ api/user/input.go
@@ -8,9 +8,17 @@ type RegisterUserInput struct {
 }
 
 type LoginInput struct {
-	Email      string `json:"email" binding:"required,email"`
-	Password   string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
 }
 type CheckEmailInput struct {
 	Email string `json:"email" binding:"required, email"`
+}
+
+type FormCreateUserInput struct {
+	Name       string `form:"name" binding:"required"`
+	Email      string `form:"email" binding:"required,email"`
+	Occupation string `form:"occupation" binding:"required"`
+	Password   string `form:"password" binding:"required"`
+	Error      error
 }
--- api/web/handler/user.go
+++ api/web/handler/user.go
@@ -24,3 +24,30 @@ func (h *userHandler) Index (c *gin.Context) {
 	}
 	c.HTML(http.StatusOK,"user_index.html",gin.H{"users":users})
 }
+
+func (h *userHandler) New (c *gin.Context) {
+	c.HTML(http.StatusOK,"user_new.html",nil)
+}
+
+func (h *userHandler) Create (c *gin.Context) {
+	var input user.FormCreateUserInput
+	err := c.ShouldBind(&input)
+	if err != nil {
+		input.Error = err
+		c.HTML(http.StatusOK, "user_new.html", input)
+		return
+	}
+	registerInput := user.RegisterUserInput {
+		Name: input.Name,
+		Email: input.Email,
+		Occupation: input.Occupation,
+		Password: input.Password,
+	}
+	_, err = h.userService.RegisterUser(registerInput)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+	c.Redirect(http.StatusFound, "/users")
+	
+}
